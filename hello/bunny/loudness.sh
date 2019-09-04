1bbc256904857afb7e718373b4b610ee2a5918e1
Loh Siu Yin <siuyin@beyondbroadcast.com>
2019-09-05 01:34:54 +0800

    modified:   loudness.sh

--- hello/bunny/loudness.sh
+++ hello/bunny/loudness.sh
@@ -3,7 +3,8 @@
 # ffplay -f lavfi -i "movie=bunny.mov:streams='a\:0',ebur128=video=1:meter=18 [out0][out1]"
 # play audio stream 0
 ffplay -f lavfi \
-	"movie=bunny.mov:streams=dv+da[vid][aud]; \
-	[vid]copy[bg]; \
-	[aud]ebur128=video=1:meter=18[fg][out1]; \
-	[bg][fg]overlay=x='W/2':y='H/2'[out0]"
+	"movie=bunny.mov:streams=dv+da[vid][aud]; 
+	[vid]copy[bg]; 
+	[aud]ebur128=video=1:meter=18[fg][out1]; 
+	[fg]format=rgba,colorchannelmixer=aa=0.5[fgt];
+	[bg][fgt] overlay [out0]"
