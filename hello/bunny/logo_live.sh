fd576a6f3e0d235d3b91a50e12c7eeb1822ecdfc
Loh Siu Yin <siuyin@beyondbroadcast.com>
2019-09-05 12:30:23 +0800

    modified:   logo_live.sh

--- hello/bunny/logo_live.sh
+++ hello/bunny/logo_live.sh
@@ -6,5 +6,5 @@ if [ "$2" != "" ]; then l=$2; fi
 #ffmpeg -i "$f" -i "$l" -lavfi "overlay=x=main_w/2" bunny_logo.mp4
 ffplay -f lavfi "movie=$f:streams=dv+da[bg][aud];
   movie=$l[logo];
-  [logo]scale=w=iw/3:-1[logos];
+  [logo]scale=w=iw/3:-1,format=rgba,colorchannelmixer=aa=0.5[logos];
   [bg][logos]overlay=x=0.975*W-w:y=0.05*H[out0],[aud]acopy[out1]"
