8554bbbd3905193f1a0873f1b280e8307694e20b
Loh Siu Yin <siuyin@beyondbroadcast.com>
2019-09-05 10:39:17 +0800

    modified:   loudness_report.sh

--- hello/bunny/loudness_report.sh
+++ hello/bunny/loudness_report.sh
@@ -2,6 +2,6 @@
 f="bunny.mov"
 if [ "$1" != "" ]; then f=$1; fi
 echo $f >loudness_report.txt
-ffmpeg -hide_banner -i $f  -af "ebur128" -f null - 2>&1|grep -A15 Integrated >> loudness_report.txt
+ffmpeg -hide_banner -i "$f"  -af "ebur128" -f null - 2>&1|grep -A15 Integrated >> loudness_report.txt
 echo loudness_report.txt generated.
 
