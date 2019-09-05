98a6ab48e3bd84bafea23167eb9b7b067741569d
Loh Siu Yin <siuyin@beyondbroadcast.com>
2019-09-05 10:33:58 +0800

    new file:   loudness_report.sh

---
+++ hello/bunny/loudness_report.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+f="bunny.mov"
+if [ "$1" != "" ]; then f=$1; fi
+echo $f >loudness_report.txt
+ffmpeg -hide_banner -i $f  -af "ebur128" -f null - 2>&1|grep -A15 Integrated >> loudness_report.txt
+echo loudness_report.txt generated.
+
