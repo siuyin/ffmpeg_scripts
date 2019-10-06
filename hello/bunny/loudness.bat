8241acd756159ca917a16975290d86d00b4eb697
siuyin <siuyin@beyondbroadcast.com>
2019-10-07 00:54:04 +0800

    Update loudness.bat

--- hello/bunny/loudness.bat
+++ hello/bunny/loudness.bat
@@ -1,4 +1,10 @@
 setlocal enableextensions
 set f=bunny.mov
 if not "%1" == "" set f=%1
-ffplay -f lavfi "movie=%f%:streams=dv+da[vid][aud]; [vid]copy[bg]; [aud]ebur128=video=1:meter=18[fg][out1]; [fg]format=rgba,colorchannelmixer=aa=0.7[fgt]; [bg][fgt] overlay [out0]"
+echo %f%
+ffplay -f lavfi ^
+"movie=%f%:streams=dv+da[vid][aud]; ^
+[vid]copy[bg]; ^
+[aud]ebur128=video=1:meter=18[fg][out1]; ^
+[fg]format=rgba,colorchannelmixer=aa=0.7[fgt]; ^
+[bg][fgt] overlay [out0]"
