27439426ebcbde7cdd5924a2a5c08904906bf7b7
Loh Siu Yin <siuyin@beyondbroadcast.com>
2021-07-02 17:33:04 +0000

    feat: allow file selection for vectorscope

--- hello/bunny/vectorscope.sh
+++ hello/bunny/vectorscope.sh
@@ -6,4 +6,6 @@
 # format is the pixel format of the vectorscope overlay.
 # for formats list: ffmpeg -pix_fmt | less
 #ffplay -i bunny.mov -vf "format=yuva444p9,split=2[m][v],[v]vectorscope=b=0.5:m=color3:g=green[v],[m][v]overlay=x=W-w:y=H-h"
-ffplay -i bunny.mov -vf "format=yuv44p9,split=2[m][v],[v]vectorscope=b=0.1:o=1.0:m=color3:g=color[v],[m][v]overlay=x=W-w:y=H-h"
+f="bunny.mov"
+if [ "$1" != "" ] ;then f=$1; fi
+ffplay -i $f -vf "format=yuv444p9,split=2[m][v],[v]vectorscope=b=0.1:o=1.0:m=color3:g=color[v],[m][v]overlay=x=W-w:y=H-h"
