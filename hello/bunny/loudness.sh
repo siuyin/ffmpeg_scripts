119ec24adb41a010dcb6e87ec2c9d64e4b750aa2
Loh Siu Yin <siuyin@beyondbroadcast.com>
2021-07-02 17:48:57 +0000

    doc: loudness.sh m=momentary

--- hello/bunny/loudness.sh
+++ hello/bunny/loudness.sh
@@ -2,6 +2,7 @@
 #ffplay -f lavfi -i "amovie=bunny.mov,ebur128=video=1:meter=18 [out0][out1]"
 # ffplay -f lavfi -i "movie=bunny.mov:streams='a\:0',ebur128=video=1:meter=18 [out0][out1]"
 # play audio stream 0
+# By default, it logs a message at a frequency of 10Hz with the Momentary loudness (identified by M), Short-term loudness (S), Integrated loudness (I) and Loudness Range (LRA).
 f="bunny.mov"
 if [ "$1" != "" ] ;then f=$1; fi
 ffplay -f lavfi \
