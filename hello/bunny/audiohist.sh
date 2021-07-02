29ed5622644c097a3d53c3d9f7c2d76b5e01a1ac
Loh Siu Yin <siuyin@beyondbroadcast.com>
2021-07-02 19:55:48 +0000

    fix: set audio histogram to single channel

--- hello/bunny/audiohist.sh
+++ hello/bunny/audiohist.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # audio histogram
 # draw: dot|line
+# dmode: single|separate single=single histogram for all channels
 f="bunny.mov"
 if [ "$1" != "" ] ;then f=$1; fi
 ffplay -f lavfi "amovie=$f, asplit [a][out1];
-             [a] ahistogram=dmode=separate:size=800x800 [out0]"
+             [a] ahistogram=dmode=single:size=800x800 [out0]"
