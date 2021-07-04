57971070718a090f690abb6fa93dc1496d5e32d3
Loh Siu Yin <siuyin@beyondbroadcast.com>
2021-07-04 16:28:25 +0000

    fix: audioscope trace to dots

--- hello/bunny/audioscope.sh
+++ hello/bunny/audioscope.sh
@@ -4,4 +4,4 @@
 f="bunny.mov"
 if [ "$1" != "" ] ;then f=$1; fi
 ffplay -f lavfi "amovie=$f, asplit [a][out1];
-             [a] avectorscope=draw=line:size=800x800 [out0]"
+             [a] avectorscope=draw=dot:size=800x800 [out0]"
