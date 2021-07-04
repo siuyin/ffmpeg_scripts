351be6dcd8e9f57c420a7b206d2f2b027ad61dd5
Loh Siu Yin <siuyin@beyondbroadcast.com>
2021-07-04 00:26:24 +0000

    feat: make bars_and_tone configurable

--- hello/bunny/bars_and_tone.sh
+++ hello/bunny/bars_and_tone.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-ffplay -f lavfi "sine=frequency=1000:sample_rate=48000[out1];smptebars=size=1280x720[out0]"
+
+b="pal75bars" # pal100bars, smptebars, smptehdbars
+if [ "$1" != "" ] ;then b=$1; fi
+#ffplay -f lavfi "sine=frequency=1000:sample_rate=48000[out1];smptebars=size=1280x720[out0]"
+ffplay -loglevel quiet -f lavfi "sine=frequency=1000:sample_rate=48000[out1];$b=size=1280x720[out0]" 
