3e84b38289ec287c9d8bdd0f61db0d15cf635848
Loh Siu Yin <siuyin@beyondbroadcast.com>
2021-07-11 11:54:03 +0800

    feat: update waveform.sh to select components

--- hello/bunny/waveform.sh
+++ hello/bunny/waveform.sh
@@ -5,6 +5,8 @@
 # tw scope trace width
 f="bunny.mov"
 y=545
+c=1
 if [ "$1" != "" ] ;then f=$1; fi
 if [ "$2" != "" ] ;then y=$2; fi
-ffplay -i $f  -vf oscilloscope=c=1:tw=0.9:y=$y/1080:o=0.5
+if [ "$3" != "" ] ;then c=$3; fi
+ffplay -i $f  -vf oscilloscope=c=${c}:tw=0.9:y=$y/1080:o=0.5
